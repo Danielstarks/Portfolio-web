db25f6d5d33e55b052b117340dd807766a8bb08c
Danielstark <kamirates@gmail.com>
2022-06-17 09:05:05 -0300

    novo commit

---
+++ site2/index.js
@@ -0,0 +1,7 @@
+let menu = documento.querySelector('#menu-icons');
+let navlist = documento.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
