f2c4b5aa16fb0238b060e418ab4dd55123ef4788
Danielstark <kamirates@gmail.com>
2022-06-19 16:04:59 -0300

    ok

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header .container');
+const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click',()=>{
+hamburger.addEventListener('click',() =>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
 
-document .addEventListener('scroll' ,()=>{
+document.addEventListener('scroll',() =>{
     var scroll_position = window.scrollY;
-    if(scroll_position>250){
-        header.style.backgraundColor = "#29323c";
+    if(scroll_position > 250){
+        header.style.backgraundColor = '#29323c';
     }else{
-        header.style.backgraund = "trans"
+        header.style.backgraund = 'transparent';
     }
-} );
+});
